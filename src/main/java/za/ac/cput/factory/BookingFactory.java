7e03e640465c8e4f175846f4e0978b8d088ba887
charmaine-dlamini-18 <222056401@mycput.ac.za>
2026-05-10 23:31:32 +0200

    Booking Factory

--- src/main/java/za/ac/cput/factory/BookingFactory.java
+++ src/main/java/za/ac/cput/factory/BookingFactory.java
@@ -9,36 +9,34 @@ import za.ac.cput.domain.Booking;
 import za.ac.cput.domain.Payment;
 import za.ac.cput.util.Helper;
 
+import java.time.LocalDateTime;
+
 public class BookingFactory {
-    public static Booking createBooking(String bookingId, String sessionId,
-                                        String sessionTime, String sessionType,
-                                        String duration, String studentNumber,
-                                        String subjectCode, String tutorId,
-                                        Payment payment){
+    public static Booking createBooking(String bookingId, String studentNumber,
+                                        String subjectCode, String sessionType, String duration,
+                                        String tutorId, LocalDateTime date){
 
         if(Helper.isNullOrEmpty(bookingId)
-                || Helper.isNullOrEmpty(sessionId)
-                || Helper.isNullOrEmpty(sessionTime)
-                || Helper.isNullOrEmpty(sessionType)
-                || Helper.isNullOrEmpty(duration)
                 || Helper.isNullOrEmpty(studentNumber)
                 || Helper.isNullOrEmpty(subjectCode)
+                || Helper.isNullOrEmpty(sessionType)
+                || Helper.isNullOrEmpty(duration)
                 || Helper.isNullOrEmpty(tutorId)
-                || Helper.isNull(payment)
-        ){
 
+        ){
+            return null;
+        }
+        if(Helper.isNull(date)){
             return null;
         }
         return new Booking.Builder()
                 .setBookingId(bookingId)
-                .setSessionId(sessionId)
-                .setSessionTime(sessionTime)
-                .setSessionType(sessionType)
-                .setDuration(duration)
                 .setStudentNumber(studentNumber)
                 .setSubjectCode(subjectCode)
+                .setSessionType(sessionType)
+                .setDuration(duration)
                 .setTutorId(tutorId)
-                .setPayment(payment)
+                .setDate(date)
                 .build();
 
     }
