eca72f501ce582047af49ce280a4e51850c1da1b
sabelo-ceza-26 <220094489@mycput.ac.za>
2026-06-27 23:30:17 +0200

    TutorSubject Repository Updated

--- src/main/java/za/ac/cput/repository/TutorSubjectRepository.java
+++ src/main/java/za/ac/cput/repository/TutorSubjectRepository.java
@@ -1,80 +1,10 @@
 package za.ac.cput.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import za.ac.cput.domain.TutorSubject;
 import za.ac.cput.domain.TutorSubjectId;
 
-import java.util.ArrayList;
-import java.util.List;
-
-/*
-TutorSubject.java
-TutorSubject Repository class
-Author: Charmaine Dlamini - 222056401
-Date: 1/05/2026
-*/
-
-public class TutorSubjectRepository implements ITutorSubjectRepository {
-
-    private static ITutorSubjectRepository repository = null;
-    private List<TutorSubject> tutorSubjects;
-
-    private TutorSubjectRepository() {
-        tutorSubjects = new ArrayList<>();
-    }
-    public static ITutorSubjectRepository getRepository() {
-        if (repository == null) {
-            repository = new TutorSubjectRepository();
-        }
-        return repository;
-    }
-
-    @Override
-    public TutorSubject create(TutorSubject tutorSubject) {
-        boolean success = tutorSubjects.add(tutorSubject);
-        if (success) {
-            return tutorSubject;
-        }
-        return null;
-    }
-
-    @Override
-    public TutorSubject read(TutorSubjectId id) {
-
-        for (TutorSubject tutorSubject : tutorSubjects) {
-
-            if (tutorSubject.getSubjectCode().equals(id.getSubjectCode())
-                    && tutorSubject.getTutorId().equals(id.getTutorId())) {
-
-                return tutorSubject;
-            }
-        }
-
-        return null;
-    }
-    @Override
-    public TutorSubject update(TutorSubject tutorSubject) {
-        TutorSubjectId id  = new TutorSubjectId(
-                tutorSubject.getSubjectCode(),
-                tutorSubject.getTutorId());
-        TutorSubject oldTutorSubject = read(id);
-        if (oldTutorSubject != null) {
-            tutorSubjects.remove(oldTutorSubject);
-            tutorSubjects.add(tutorSubject);
-            return tutorSubject;
-        }
-        return null;
-    }
-    @Override
-    public boolean delete(TutorSubjectId  id) {
-        TutorSubject tutorSubjectToDelete = read(id);
-        if (tutorSubjectToDelete != null) {
-            tutorSubjects.remove(tutorSubjectToDelete);
-            return true;
-        }
-        return false;
-    }
-    @Override
-    public List<TutorSubject> getAll() {
-        return tutorSubjects;
-    }
-
+@Repository
+public interface TutorSubjectRepository extends JpaRepository<TutorSubject, TutorSubjectId> {
 }
